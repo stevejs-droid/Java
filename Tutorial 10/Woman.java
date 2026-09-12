a2bd8116bf7b8fc9453da9eaca9cab5dff8570b5
Steve J. Stephen <stevealpy@gmail.com>
2026-09-12 19:57:15 +0530

    Create Woman.java

---
+++ Tutorial 10/Woman.java
@@ -0,0 +1,15 @@
+package pack2;
+
+public class Woman {
+    private String name;
+    private int age;
+
+    public Woman(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayMe() {
+        System.out.println("Woman's Name: " + name + ", Age: " + age);
+    }
+}
